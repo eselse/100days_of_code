1bd90083bd067dc3ce1ec686dff997d78b35d392
eselse <ilikewebdevelopment@gmail.com>
2022-06-03 13:18:54 +0400

    Convert lists to sets

--- PyPassword_generator.py
+++ PyPassword_generator.py
@@ -1,28 +1,23 @@
 #Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
+symbols = {'!', '#', '$', '%', '&', '(', ')', '*', '+'}
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
 password_length = sum([nr_letters, nr_symbols, nr_numbers])
 
-numbers_list = [random.choice(numbers) for number in range(0, nr_numbers)]
-symbols_list = [random.choice(symbols) for symbol in range(0, nr_symbols)]
-letters_list = [random.choice(letters) for letter in range(0, nr_letters)]
+password_list = []
 
-password_list = [*letters_list, *numbers_list, *symbols_list]
+numbers_list = [password_list.append(random.choice(list(numbers))) for number in range(0, nr_numbers)]
+symbols_list = [password_list.append(random.choice(list(symbols))) for symbol in range(0, nr_symbols)]
+letters_list = [password_list.append(random.choice(list(letters))) for letter in range(0, nr_letters)]
 
 random.shuffle(password_list)
 
