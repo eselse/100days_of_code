6663570a7690bf6e1c47b7d9f0ac21ea158cec67
eselse <ilikewebdevelopment@gmail.com>
2022-06-03 16:24:48 +0400

    Add solution for Maze problem

---
+++ Maze.py
@@ -0,0 +1,16 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def move_around():
+#     turn_right()
+#     move()
+#     turn_right()
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#     if not wall_on_right() and not at_goal():
+#         move_around()
